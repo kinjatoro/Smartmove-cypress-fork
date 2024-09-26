8db0c5b6bc0465b1cb0d1ba33e00ef2703b8dde8
kinjatoro <136925952+kinjatoro@users.noreply.github.com>
2024-09-26 20:18:57 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:8080", // aca va la url base
+    baseUrl: "https://main.djeml4cm1lzmh.amplifyapp.com/", // aca va la url base
   },
 });
