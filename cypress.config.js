b2bc61ab7ae72b9ee5897b303ac4d2458b61cce8
kinjatoro <136925952+kinjatoro@users.noreply.github.com>
2024-09-26 21:29:11 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'junit', // Usamos el reporte de JUnit
+  reporterOptions: {
+    mochaFile: 'cypress/results/test-results.[hash].xml', // Ruta de los archivos de resultados
+    toConsole: true // Opcional: imprime los resultados en la consola
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implementar node event listeners aquí si es necesario
     },
-    baseUrl: "https://main.djeml4cm1lzmh.amplifyapp.com/", // aca va la url base
+    baseUrl: "https://main.djeml4cm1lzmh.amplifyapp.com/", // URL base
   },
 });
